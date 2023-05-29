5d587f936cf0e85d342c2240c1d7315d26d1a9d1
mwmarszalek <mw.marszalek@outlook.com>
2023-05-29 10:43:21 +0100

    add axiom get, add CORS policy

--- API/Program.cs
+++ API/Program.cs
@@ -21,6 +21,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,6 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
