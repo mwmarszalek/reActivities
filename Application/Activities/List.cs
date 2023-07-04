c8c39a6a884e77ffd8a787f81f86d4686732944e
mwmarszalek <mw.marszalek@outlook.com>
2023-07-04 09:30:37 +0100

    remove cancellation tokens

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -17,30 +17,14 @@ namespace Application.Activities
         {
 
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
             
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
-                _logger = logger;
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<List<Activity>> Handle(Query request, CancellationToken token)
             {
-                try
-                {
-                    for (var i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (System.Exception)
-                {
-                    _logger.LogInformation("Task was cancelled");
-                }
-
                 return await _context.Activities.ToListAsync();
             }
         }
