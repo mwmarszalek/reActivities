5a8d02c591a3e308a4a74d5bb0568e6f0111cad2
mwmarszalek <mw.marszalek@outlook.com>
2023-07-04 09:27:17 +0100

    add cancellation tokens to test

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,7 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -16,14 +17,30 @@ namespace Application.Activities
         {
 
             private readonly DataContext _context;
+            private readonly ILogger<List> _logger;
             
-            public Handler(DataContext context)
+            public Handler(DataContext context, ILogger<List> logger)
             {
+                _logger = logger;
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+                try
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed");
+                    }
+                }
+                catch (System.Exception)
+                {
+                    _logger.LogInformation("Task was cancelled");
+                }
+
                 return await _context.Activities.ToListAsync();
             }
         }
